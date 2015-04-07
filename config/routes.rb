02e548ce07a3231e1cd0b397f8652908dfaf90a6
taylorsun1 <taylor.sun@verizon.net>
2015-04-07 10:41:33 -0700

    Added ability to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :pics
   devise_for :users
   root "pics#index"
   get "about" => "pages#about"
+  get "posts" => "pages#posts"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
