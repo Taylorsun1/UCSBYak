598cc6e9ca788641ed66a638738e11c87fca82d0
taylorsun1 <taylor.sun@verizon.net>
2015-04-06 20:23:41 -0700

    Added PicFeed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :pics
   devise_for :users
-  root "pages#home"
+  root "pics#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
