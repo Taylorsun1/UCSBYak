553c40892a08c09d5ecd65cdd2c5285e391f6beb
taylorsun1 <taylor.sun@verizon.net>
2015-04-07 09:52:34 -0700

    Add About page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :pics
   devise_for :users
   root "pics#index"
+  get "about" => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
