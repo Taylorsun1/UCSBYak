d1e13ccdea3f69df694f5299ef679b88fafa1ffa
taylorsun1 <taylor.sun@verizon.net>
2015-04-13 07:38:46 -0700

    before revert

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,6 +1,6 @@
 class Pic < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "1600x1600>", :thumb => "1600x1600>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
